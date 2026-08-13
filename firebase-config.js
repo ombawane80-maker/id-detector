cde688e43cebfd88a2020fb732c12a579bf2c512
ombawane80-maker <ombawane80@gmail.com>
2026-08-13 23:43:24 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// Replace these placeholders with your Firebase Web App config.
+export  const firebaseConfig = {
+    apiKey: "AIzaSyAjcKNUjXS57OF7KvAnYZ3GqOFZM4p0ghw",
+    authDomain: "id-detector-c38ec.firebaseapp.com",
+    projectId: "id-detector-c38ec",
+    storageBucket: "id-detector-c38ec.firebasestorage.app",
+    messagingSenderId: "269382383001",
+    appId: "1:269382383001:web:0586ed534cf73619c00f08",
+    measurementId: "G-D7CN2X1TBT"
+};
